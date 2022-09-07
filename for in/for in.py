796695f758d8358dc039919aa254dcc2a5d6465f
idrisNur <idrisxonmusulmonov95@gmail.com>
2022-09-07 14:44:34 +0500

    Create for in.py
    for lupi haqida

---
+++ for in/for in.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Редактор Spyder
+
+Это временный скриптовый файл.
+"""
+
+
+ =============================================================================
+sonlar = list(range(0,11))
+for son in sonlar:
+    print(f"{son}ning kvadrati {son**2}ga teng")
+ =============================================================================
+ dostlar = []
+ print("5ta eng yaxshi do`stingizni ayting")
+ 
+ for dost in range(5):
+     dostlar.append(input(f"{dost+1}-do`stingizni ismini kiriting\n>>"))
+ print(dostlar)    
+ =============================================================================
+ =============================================================================
+ mehmonlar = ["Ali", "Vali", "Abbos", "Sevinch", "Oisha"]
+ for mehmon in mehmonlar:
+     print(f"Hurmatli {mehmon}, sizni lutfan taklif etamiz")
+     print("Hurmat bilan o`zim\n")
+ =============================================================================
+
